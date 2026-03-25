fa65adbd0587b7057a3d5c412d58a51ab757b195
Nithin0620 <nithin30302@gmail.com>
2026-03-25 16:54:26 +0530

    ..

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -121,8 +121,7 @@ const App: React.FC = () => {
         !isAdministrative &&
         !isBloPortal &&
         !isAuditTrail &&
-        !isFuzzyDetection &&
-        !isIndiaMap && (
+        !isFuzzyDetection && (
           <nav
             className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 border-b border-transparent
           ${isScrolled
